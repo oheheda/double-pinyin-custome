ab8c795f7e663adc7678418cd07efcd7e6e2aab2
GF39 <tiyanhe@126.com>
2023-06-11 01:02:35 +0800

    完整方案

---
+++ rime.lua
@@ -0,0 +1,19 @@
+function datetime_translator(input, seg)
+    if (input == "date") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " "))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
+    elseif (input == "time") then -- 关键字更改，你也可以用or语句定义多个关键字
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " "))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分%S秒"), " "))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), " "))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分"), " "))
+    elseif (input == "datetime") then
+        -- yield(Candidate("time", seg.start, seg._end, os.date(), " "))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日 %H时%M分%S秒"), " "))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), " "))
+        -- yield(Candidate("time", seg.start, seg._end, os.date(), " "))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日 %H时%M分"), " "))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M"), " "))
+    end
+end
