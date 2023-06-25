7256fb2a5336b1ef6777b7e5bccdf0e6709845ff
GF39 <tiyanhe@126.com>
2023-06-26 02:41:55 +0800

    稍微修改了一下datetime的处理，提高了优先级避免每次date都出现在第8个候选项上面

--- rime.lua
+++ rime.lua
@@ -1,8 +1,12 @@
 function datetime_translator(input, seg)
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " "))
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
+        local cand = Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " ")
+        cand.quality = 1
+        yield(cand)
+        cand = Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " ")
+        cand.quality = 1
+        yield(cand)
     elseif (input == "time") then -- 关键字更改，你也可以用or语句定义多个关键字
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " "))
         yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分%S秒"), " "))
